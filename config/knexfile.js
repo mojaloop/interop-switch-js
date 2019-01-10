30da44019cb4af9739ff4f5f7f0e9de5c5772f22
donovan <donovanchangfoot@gmail.com>
2019-01-10 16:10:48 +0200

    add parties and quotes api

---
+++ config/knexfile.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const migrationsDirectory = '../migrations'
+const seedsDirectory = '../seeds'
+
+const Config = require('../src/lib/config')
+
+module.exports = {
+  client: 'mysql',
+  version: '5.5',
+  connection: Config.DATABASE_URI,
+  // migrations: {
+  //   directory: migrationsDirectory,
+  //   tableName: 'migration',
+  //   stub: `${migrationsDirectory}/migration.template`
+  // },
+  // seeds: {
+  //   directory: seedsDirectory,
+  //   loadExtensions: ['.js']
+  // }
+}
