26c757224cee81e4a7f6320810795f49bd037abb
donovan <donovanchangfoot@gmail.com>
2019-01-14 10:28:53 +0200

    add default red and blue moja config

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var http = require('http');
 /**
  * Get port from environment and store in Express.
  */
-
-var port = normalizePort(process.env.PORT || '3000');
+const config = require('./src/lib/config')
+var port = normalizePort(config.PORT || '3000');
 app.set('port', port);
 
 /**
