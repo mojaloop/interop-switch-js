37e9980ab6c7cb994f58f4338a5b8b9269c50c66
matthew <matt@sportraxs.com>
2019-01-23 11:10:12 +0200

    change the logging used to make it easier to debug

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require('axios')
 const participant = require('../domain/participant/index')
 const config = require('../lib/config')
-const Logger = require('@mojaloop/central-services-shared').Logger
+const Logger = require('debug')('interop-switch-js')
 const FSPIOP_CALLBACK_URL_QUOTE_POST = 'FSPIOP_CALLBACK_URL_QUOTE_POST'
 const FSPIOP_CALLBACK_URL_QUOTE_PUT = 'FSPIOP_CALLBACK_URL_QUOTE_PUT'
 const FSPIOP_CALLBACK_URL_PARTIES_PUT = 'FSPIOP_CALLBACK_URL_PARTIES_PUT'
@@ -49,7 +49,7 @@ router.get('/parties/:type/:type_id', async function (req, res, next) {
 router.post('/quotes', async function(req, res, next) {
   let headers = Object.assign({}, req.headers)
   if (!headers['fspiop-final-destination']) headers['fspiop-final-destination'] = headers['fspiop-destination']
-  Logger.debug('Received post quote request from ' + headers['fspiop-source'])
+  Logger('Received post quote request from ' + headers['fspiop-source'])
   res.status(202).end()
 
   const nextHop = await axios.get(config.ROUTING_ENDPOINT, { headers: { 'fspiop-final-destination': headers['fspiop-final-destination'] } })
@@ -58,7 +58,7 @@ router.post('/quotes', async function(req, res, next) {
   const endpointDataSet =  await participant.getEndpoint(nextHopAddress, FSPIOP_CALLBACK_URL_QUOTE_POST)
   const endpoint = endpointDataSet[0].value
 
-  Logger.debug('Forwarding post quote onto ' + nextHopAddress, 'endpoint', endpoint)
+  Logger('Forwarding post quote onto ' + nextHopAddress, 'endpoint', endpoint)
   axios.post(endpoint, req.body, { headers })
 
 });
@@ -66,7 +66,7 @@ router.post('/quotes', async function(req, res, next) {
 router.put('/quotes/:quote_id', async function(req, res, next) {
   let headers = Object.assign({}, req.headers)
   if (!headers['fspiop-final-destination']) headers['fspiop-final-destination'] = headers['fspiop-destination']
-  Logger.debug('Received put quote request from ' + headers['fspiop-source'])
+  Logger('Received put quote request from ' + headers['fspiop-source'])
 
   const nextHop = await axios.get(config.ROUTING_ENDPOINT, { headers: { 'fspiop-final-destination': headers['fspiop-final-destination'] } })
   const nextHopAddress = nextHop.data.destination
@@ -76,7 +76,7 @@ router.put('/quotes/:quote_id', async function(req, res, next) {
   const endpointTemplate = endpointDataSet[0].value
   const endpoint = endpointTemplate.replace(/{{quoteId}}/gi, req.params.quote_id)
 
-  Logger.debug('Forwarding put quote onto ' + nextHopAddress, 'endpoint', endpoint)
+  Logger('Forwarding put quote onto ' + nextHopAddress, 'endpoint', endpoint)
 
   axios.put(endpoint, req.body, { headers })
 
